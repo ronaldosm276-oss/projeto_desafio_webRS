665e54615d9e9f7918b6b21c8e31d837fd46ba9e
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 19:56:13 -0300

    feito algumas modificações e add código no js

--- js/idade.js
+++ js/idade.js
@@ -3,15 +3,15 @@ const formPessoa = document.querySelector("#formulario-pessoa")
 const divDados = document.querySelector("#div-dados")
 
 formPessoa.addEventListener('submit').(evt) => {
-evt.preventDefalt()
+    evt.preventDefalt()
 
-const dadosForm = new FormData(formPessoa)
+    const dadosForm = new FormData(formPessoa)
 
-const nome = dadosForm.get('nome')
-const idade = dadosForm.get('idade')
+    const nome = dadosForm.get('nome')
+    const idade = dadosForm.get('idade')
 
-let situacaoIdade = idade >= 18 ? `${nome}. é Maior de idade` : `${nome}, é menor de idade`
-divDados.innerHTML = situacaoIdade
+    let situacaoIdade = idade >= 18 ? `${nome}. é Maior de idade` : `${nome}, é menor de idade`
+    divDados.innerHTML = situacaoIdade
 
 
 }
---
+++ js/scrips-fundamentos.js
@@ -0,0 +1,28 @@
+// This is a comment, this a test just to see it works
+
+/*this is a comment
+that works
+on multiple
+lines, just so
+I can see it with multiple lines, for some reason
+
+*/
+
+//COMANDO DE SAÍDA
+console.log("Hello World")
+
+//DECLARAÇÂO DE VARIÁVEL
+
+let num = 10
+var num2 = 99
+const num3 = 250
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    
+}
+
+// ALT + SHIFT + DOWN ARROW
